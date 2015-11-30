e0583cb971f363cb21f764ae7b7f4e888e60dea8
sunliying <1174587076@qq.com>
2015-11-30 23:25:36 +0800

    muxi_blog_fe

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Muxi_blog_FE
 front end template of muxi blog
+
+## build
+在src/执行  npm install
+安装依赖 
+在src/scss/下执行命令  npm run build 
+监听scss,并且自动生成css
+##数据生成
+在src/下执行命令 node mock_io.js
+生成json数据
+## run
+在app/下执行命令  python app.py
--- app/src/mock_template/index.js
+++ app/src/mock_template/index.js
@@ -1,28 +1,22 @@
 exports.obj = {
-	"blog_list|4": [
-	{
-		"title":"this is a blog",
-		"img_url":"../../../static/img/photo.png",
-		"name":"admin",
-		"date": "2015/11/22",
-		"content":"我们从这个文章中进一步分析一下，……2015年1我们从这个文章中进一步分析一下，……2015年11我们从这个文章中进一步分析一下，……2015年1月16日 - 图片不代表产品,PX-22PX-22PX-22PX-22如需详细对应资料及图片请咨询本公司。 基恩士KEYENCE是国际型气动品牌,是全球使用范围最广,知名度最高的作为传感器和测量...",		
-		
-		"adatar":"../../../static/img/side_img.png",
-		"like_number":"99",
-		"comment_number":"10"
-	}
-	],
-	"tag|3":[
-	{'value':'视觉设计'}
-	],
-	"article_tag|12":[
-	{
-		"value":"视觉设计"
-	}
-	],
-	"article_date|6":[
-	{
-		"value":"2015年8月"
-	}
-	]
+	"blog_list|4": [{
+	    "title": "this is a blog",
+	    "img_url": "../../../static/img/photo.png",
+	    "name": "admin",
+	    "date": "2015/11/22",
+	    "content": "我们从这个文章中进一步分析一下，……2015年1我们从这个文章中进一步分析一下，……2015年11我们从这个文章中进一步分析一下，……2015年1月16日 - 图片不代表产品,PX-22PX-22PX-22PX-22如需详细对应资料及图片请咨询本公司。 基恩士KEYENCE是国际型气动品牌,是全球使用范围最广,知名度最高的作为传感器和测量...",
+	    "adatar": "../../../static/img/side_img.png",
+	    "like_number": "99",
+	    "comment_number": "10"
+	}],
+	"tag|3": [{
+	    'value': '视觉设计'
+	}],
+	"article_tag|12": [{
+	    "value": "视觉设计"
+	}],
+	"article_date|6": [{
+	    "value": "2015年8月"
+	}]
+
 };
